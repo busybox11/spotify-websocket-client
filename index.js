7eb9eb870d4d7f87733bb2b848c201eee27d5986
busybox <planetgeek11@gmail.com>
2021-08-30 23:59:50 +0200

    Added initial files

---
+++ index.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+var SpotifyWebApi = require('spotify-web-api-node')
+
+var spotifyApi = new SpotifyWebApi({
+	clientId: process.env.SPOTIFY_CLIENT_ID,
+	clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+	redirectUri: process.env.SPOTIFY_REDIRECT_URI
+})
+
+spotifyApi.setRefreshToken(process.env.REFRESH_TOKEN);
+spotifyApi.refreshAccessToken().then(
+	function(data) {
+		console.log('The access token has been refreshed!');
+  
+		// Save the access token so that it's used in future calls
+		spotifyApi.setAccessToken(data.body['access_token']);
+
+		spotifyApi.getMySavedTracks()
+		.then(function(data) {
+			console.log(data.body.items);
+		}, function(err) {
+			console.log('Something went wrong!', err);
+		});
+	},
+	function(err) {
+		console.log('Could not refresh access token', err);
+	}
+);
