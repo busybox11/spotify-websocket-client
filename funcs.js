7897b47d04de89fac0a20dc61d3243d8e78c592f
busybox <planetgeek11@gmail.com>
2021-08-31 01:34:26 +0200

    Added song check feature

---
+++ funcs.js
@@ -0,0 +1,30 @@
+const { spotifyApi, newToken, lastSongs } = require('./index')
+
+async function getPlayingData() {
+	try {
+		let data = await spotifyApi.getMyCurrentPlaybackState()
+		return data.body
+	} catch(e) {
+		console.log('Something went wrong!', e);
+		newToken();
+	}
+}
+
+async function getPlayingSongName() {
+	let data = await getPlayingData()
+	try {
+		songName = `${data.item.name} - ${data.item.artists[0].name}`
+	} catch(e) {
+		songName = 'No song currently playing'
+	}
+
+	lastSongs[1] = lastSongs[0]
+	lastSongs[0] = songName
+
+	return songName
+}
+
+module.exports = {
+	getPlayingData,
+	getPlayingSongName
+}
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 require('dotenv').config()
+const { WebSocket, WebSocketServer } = require('ws');
 var SpotifyWebApi = require('spotify-web-api-node')
+let tokenExpirationEpoch;
 
 var spotifyApi = new SpotifyWebApi({
 	clientId: process.env.SPOTIFY_CLIENT_ID,
@@ -8,21 +10,69 @@ var spotifyApi = new SpotifyWebApi({
 })
 
 spotifyApi.setRefreshToken(process.env.REFRESH_TOKEN);
-spotifyApi.refreshAccessToken().then(
-	function(data) {
-		console.log('The access token has been refreshed!');
-  
-		// Save the access token so that it's used in future calls
-		spotifyApi.setAccessToken(data.body['access_token']);
 
-		spotifyApi.getMySavedTracks()
-		.then(function(data) {
-			console.log(data.body.items);
-		}, function(err) {
-			console.log('Something went wrong!', err);
-		});
-	},
-	function(err) {
-		console.log('Could not refresh access token', err);
-	}
-);
+function newToken() {
+	console.log('Refreshing token')
+	spotifyApi.setRefreshToken(process.env.REFRESH_TOKEN);
+	spotifyApi.refreshAccessToken().then(
+		function(data) {
+			console.log('The access token has been refreshed!');
+	  
+			// Save the access token so that it's used in future calls
+			spotifyApi.setAccessToken(data.body['access_token']);
+	
+			tokenExpirationEpoch =
+				new Date().getTime() / 1000 + data.body['expires_in'];
+	
+			console.log(
+				'Refreshed token. It expires in ' +
+				Math.floor(tokenExpirationEpoch - new Date().getTime() / 1000) +
+				' seconds!'
+			);
+		},
+		function(err) {
+			console.log('Could not refresh access token', err);
+		}
+	);
+}
+
+newToken();
+
+var lastSongs = ["", ""];
+
+module.exports = {
+	spotifyApi,
+	newToken,
+	lastSongs
+}
+
+var funcs = require('./funcs')
+
+tokenRefreshInterval = setInterval(newToken, 1000 * 60 * 59);
+
+const wss = new WebSocketServer({
+	port: 35678
+});
+
+async function songLoop() {
+	try {
+		let name = await funcs.getPlayingSongName()
+		if (name != lastSongs[1]) {
+			wss.clients.forEach(function each(client) {
+				if (client.readyState === WebSocket.OPEN) {
+					client.send(JSON.stringify({type: 'updatedSong', song: name}));
+				}
+			});
+		}
+	} catch(e) {}
+}
+
+wss.on('connection', async function connection(ws) {
+	ws.on('message', function incoming(message) {
+		console.log('received: %s', message);
+	});
+
+	ws.send(await funcs.getPlayingSongName())
+});
+
+tokenRefreshInterval = setInterval(async () => { songLoop() }, 3 * 10 * 60);
