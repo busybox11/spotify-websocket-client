5fc3f9277540f854ae9f1c5b235cc1c880abf3bd
busybox11 <busybox11th0gmail.com>
2023-08-08 15:40:57 +0000

    app: Prettier

--- index.js
+++ index.js
@@ -1,157 +1,163 @@
 // Initialize the .env file and the environment variables of the user
-require('dotenv').config()
+require("dotenv").config();
 
-const { WebSocket, WebSocketServer } = require('ws')
-var SpotifyWebApi = require('spotify-web-api-node')
+const { WebSocket, WebSocketServer } = require("ws");
+var SpotifyWebApi = require("spotify-web-api-node");
 
-let tokenExpirationEpoch
+let tokenExpirationEpoch;
 
 // Set all constants of the API from the .env file (or environment variables)
 var spotifyApi = new SpotifyWebApi({
-        clientId: process.env.SPOTIFY_CLIENT_ID,
-        clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-        redirectUri: process.env.SPOTIFY_REDIRECT_URI
-})
+  clientId: process.env.SPOTIFY_CLIENT_ID,
+  clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+  redirectUri: process.env.SPOTIFY_REDIRECT_URI,
+});
 
 // Set the refresh token from the .env file (or environment variables)
 // If that doesn't work, it means that the user either hasn't logged in
 // properly, or at all, and thus needs to do it to set the refresh token
-spotifyApi.setRefreshToken(process.env.REFRESH_TOKEN)
+spotifyApi.setRefreshToken(process.env.REFRESH_TOKEN);
 
 function newToken() {
-        // Function that refreshes the access token
-        // Called every 59 minutes, and when needed (startup, some errors)
-        console.log('Refreshing token')
+  // Function that refreshes the access token
+  // Called every 59 minutes, and when needed (startup, some errors)
+  console.log("Refreshing token");
 
-        // Set the refresh token from the environment, just in case
-        spotifyApi.setRefreshToken(process.env.REFRESH_TOKEN)
-        spotifyApi.refreshAccessToken().then(
-                function(data) {
-                        // Save the access token so that it's used in future calls
-                        spotifyApi.setAccessToken(data.body['access_token'])
+  // Set the refresh token from the environment, just in case
+  spotifyApi.setRefreshToken(process.env.REFRESH_TOKEN);
+  spotifyApi.refreshAccessToken().then(
+    function (data) {
+      // Save the access token so that it's used in future calls
+      spotifyApi.setAccessToken(data.body["access_token"]);
 
-                        /*
-                        // Save the expiration time to know when is the token expiring,
-                        // just in case
-                        tokenExpirationEpoch =
-                                new Date().getTime() / 1000 + data.body['expires_in']
+      /*
+      // Save the expiration time to know when is the token expiring,
+      // just in case
+      tokenExpirationEpoch =
+        new Date().getTime() / 1000 + data.body['expires_in']
 
-                        // Logs that the token has been refreshed, with the expiration time
-                        // showed in seconds
-                        console.log(
-                                'Refreshed token. It expires in ' +
-                                Math.floor(tokenExpirationEpoch - new Date().getTime() / 1000) +
-                                ' seconds!'
-                        )
-                        */
-                },
-                function(err) {
-                        console.log('Could not refresh access token', err)
-                }
-        )
+      // Logs that the token has been refreshed, with the expiration time
+      // showed in seconds
+      console.log(
+        'Refreshed token. It expires in ' +
+        Math.floor(tokenExpirationEpoch - new Date().getTime() / 1000) +
+        ' seconds!'
+      )
+      */
+    },
+    function (err) {
+      console.log("Could not refresh access token", err);
+    }
+  );
 }
 
 // Renew the token at each startup
-newToken()
+newToken();
 
 // Initialize an empty last songs array containing the last played track
 // and its artist, and the last playing position
-var lastSongs = ["", ""]
-var lastPosition = 0
-var lastIsPlaying = false
+var lastSongs = ["", ""];
+var lastPosition = 0;
+var lastIsPlaying = false;
 
 module.exports = {
-        spotifyApi,
-        newToken,
-        lastSongs
-}
+  spotifyApi,
+  newToken,
+  lastSongs,
+};
 
-var funcs = require('./funcs')
+var funcs = require("./funcs");
 
 // Refreshes the access token every 59 minutes, the limit of use being
 // at 60 (1 hour)
-tokenRefreshInterval = setInterval(newToken, 1000 * 60 * 59)
+tokenRefreshInterval = setInterval(newToken, 1000 * 60 * 59);
 
 // Host the server's websocket on the port 35678
 const wss = new WebSocketServer({
-        port: 35678
-})
+  port: 35678,
+});
 
 async function songLoop() {
-        // Function called every 3 seconds to check the currently playing song
-        try {
-                // Tries to get the song, if it doesn't work, skip the request
-                let data = await funcs.getPlayingData()
-                let name = await funcs.getPlayingSongName(data)
-                if (name != lastSongs[1] || Math.abs(data.progress_ms - lastPosition) > 5000 || lastIsPlaying != data.is_playing) {
-                        wss.clients.forEach(function each(client) {
-                                // Send to every websocket client the song formatted in JSON with the
-                                // type 'updatedSong', recognized as a periodic check
-                                if (client.readyState === WebSocket.OPEN) {
-                                        client.send(JSON.stringify(
-                                                {
-                                                        type: 'updatedSong',
-                                                        song: name,
-                                                        artist: data.item.artists[0].name,
-                                                        name: data.item.name,
-                                                        album: data.item.album.name,
-                                                        albumArt: data.item.album.images[0].url,
-                                                        id: data.item.id,
-                                                        progress: {
-                                                                playing: data.is_playing,
-                                                                current: data.progress_ms,
-                                                                duration: data.item.duration_ms
-                                                        }
-                                                }
-                                        ))
-                                } else {
-                                        client.close()
-                                }
-                        })
-                }
+  // Function called every 3 seconds to check the currently playing song
+  try {
+    // Tries to get the song, if it doesn't work, skip the request
+    let data = await funcs.getPlayingData();
+    let name = await funcs.getPlayingSongName(data);
+    if (
+      name != lastSongs[1] ||
+      Math.abs(data.progress_ms - lastPosition) > 5000 ||
+      lastIsPlaying != data.is_playing
+    ) {
+      wss.clients.forEach(function each(client) {
+        // Send to every websocket client the song formatted in JSON with the
+        // type 'updatedSong', recognized as a periodic check
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(
+            JSON.stringify({
+              type: "updatedSong",
+              song: name,
+              artist: data.item.artists[0].name,
+              name: data.item.name,
+              album: data.item.album.name,
+              albumArt: data.item.album.images[0].url,
+              id: data.item.id,
+              progress: {
+                playing: data.is_playing,
+                current: data.progress_ms,
+                duration: data.item.duration_ms,
+              },
+            })
+          );
+        } else {
+          client.close();
+        }
+      });
+    }
 
-                // Register last playing position
-                lastPosition = data.progress_ms
-                lastIsPlaying = data.is_playing
-        } catch(e) {}
+    // Register last playing position
+    lastPosition = data.progress_ms;
+    lastIsPlaying = data.is_playing;
+  } catch (e) { }
 }
 
-wss.on('connection', async function connection(ws) {
-        // When a new client is connecting
-        ws.on('message', async function incoming(message) {
-                // When a client sends a message to the websocket
-                console.log('received: %s', message)
-                msg = JSON.parse(message)
+wss.on("connection", async function connection(ws) {
+  // When a new client is connecting
+  ws.on("message", async function incoming(message) {
+    // When a client sends a message to the websocket
+    console.log("received: %s", message);
+    msg = JSON.parse(message);
 
-                // If the message type is a control action,
-                // use the controls handler to trigger the wanted action
-                if (msg.type == "controls") {
-                        // Awaiting the controls handler because we need to send the
-                        // new song info right after it, thus we want the handler to
-                        // be blocking
-                        await funcs.controls(msg.data)
-                        songLoop()
-                }
-        })
+    // If the message type is a control action,
+    // use the controls handler to trigger the wanted action
+    if (msg.type == "controls") {
+      // Awaiting the controls handler because we need to send the
+      // new song info right after it, thus we want the handler to
+      // be blocking
+      await funcs.controls(msg.data);
+      songLoop();
+    }
+  });
 
-        // Send the currently playing song to the new client only
-        let data = await funcs.getPlayingData()
-        ws.send(JSON.stringify(
-                {
-                        type: 'updatedSong',
-                        song: await funcs.getPlayingSongName(data),
-                        artist: data.item.artists[0].name,
-                        name: data.item.name,
-                        album: data.item.album.name,
-                        albumArt: data.item.album.images[0].url,
-                        id: data.item.id,
-                        progress: {
-                                playing: data.is_playing,
-                                current: data.progress_ms,
-                                duration: data.item.duration_ms
-                        }
-                }
-        ))
-})
+  // Send the currently playing song to the new client only
+  let data = await funcs.getPlayingData();
+  ws.send(
+    JSON.stringify({
+      type: "updatedSong",
+      song: await funcs.getPlayingSongName(data),
+      artist: data.item.artists[0].name,
+      name: data.item.name,
+      album: data.item.album.name,
+      albumArt: data.item.album.images[0].url,
+      id: data.item.id,
+      progress: {
+        playing: data.is_playing,
+        current: data.progress_ms,
+        duration: data.item.duration_ms,
+      },
+    })
+  );
+});
 
-songLoopInterval = setInterval(async () => { songLoop() }, 5 * 10 * 60)
+songLoopInterval = setInterval(async () => {
+  songLoop();
+}, 5 * 10 * 60);
--- login.js
+++ login.js
@@ -1,68 +1,74 @@
 // Initialize the .env file and the environment variables of the user
-require('dotenv').config()
+require("dotenv").config();
 
-const readline = require('readline')
+const readline = require("readline");
 
-var SpotifyWebApi = require('spotify-web-api-node')
+var SpotifyWebApi = require("spotify-web-api-node");
 
 // Defines all scopes used by the server
 var scopes = [
-    'user-read-recently-played',
-    'user-read-playback-position',
-    'user-read-playback-state',
-    'user-modify-playback-state',
-    'user-follow-read',
-    'user-follow-modify',
-    'user-library-read',
-    'user-library-modify',
-    'playlist-modify-public',
-    'playlist-modify-private'
-]
-var state = 'login'
+  "user-read-recently-played",
+  "user-read-playback-position",
+  "user-read-playback-state",
+  "user-modify-playback-state",
+  "user-follow-read",
+  "user-follow-modify",
+  "user-library-read",
+  "user-library-modify",
+  "playlist-modify-public",
+  "playlist-modify-private",
+];
+var state = "login";
 
 // Set all constants of the API from the .env file (or environment variables)
 var spotifyApi = new SpotifyWebApi({
-    clientId: process.env.SPOTIFY_CLIENT_ID,
-    clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-    redirectUri: process.env.SPOTIFY_REDIRECT_URI
-})
+  clientId: process.env.SPOTIFY_CLIENT_ID,
+  clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+  redirectUri: process.env.SPOTIFY_REDIRECT_URI,
+});
 
 // Initialize a console input interface to get the user's inputs
 // when needed
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
+  input: process.stdin,
+  output: process.stdout,
+});
 
-console.log('To login with your Spotify account, you have to open the following URL in a web browser:\n')
+console.log(
+  "To login with your Spotify account, you have to open the following URL in a web browser:\n"
+);
 // Create an authorization URL for the user to access with their browser
 // and then permits the server to get its refresh token
-var authorizeURL = spotifyApi.createAuthorizeURL(scopes, state)
-console.log(authorizeURL)
+var authorizeURL = spotifyApi.createAuthorizeURL(scopes, state);
+console.log(authorizeURL);
 
-console.log()
-console.log("Then, after granting the permissions and logging in, paste the URl you've been redirected to here:\n")
+console.log();
+console.log(
+  "Then, after granting the permissions and logging in, paste the URl you've been redirected to here:\n"
+);
 // Retrieve user input to get the URL they've been redirected to, containing
 // the access code that permits to create a refresh token
-rl.question('URL: ', (url) => {
-    let urlcode = new URL(url)
+rl.question("URL: ", (url) => {
+  let urlcode = new URL(url);
 
-    console.log()
+  console.log();
 
-    spotifyApi.authorizationCodeGrant(urlcode.searchParams.get('code')).then(
-        function(data) {
-            // The refresh token has been created, and we're prompting the user
-            // to paste it on the .env file
-            console.log(`Paste this after REFRESH_TOKEN in the .env file:\n${data.body['refresh_token']}`)
-        },
-        function(err) {
-            // The refresh token wasn't able to be created, and we're showing
-            // the error message of the request to the user, that permits the dev to
-            // know what's going on for further debugging
-            console.log('Something went wrong!', err)
-        }
-    )
-    
-    // Close the console input interface since it's not needed anymore
-    rl.close()
-})
+  spotifyApi.authorizationCodeGrant(urlcode.searchParams.get("code")).then(
+    function (data) {
+      // The refresh token has been created, and we're prompting the user
+      // to paste it on the .env file
+      console.log(
+        `Paste this after REFRESH_TOKEN in the .env file:\n${data.body["refresh_token"]}`
+      );
+    },
+    function (err) {
+      // The refresh token wasn't able to be created, and we're showing
+      // the error message of the request to the user, that permits the dev to
+      // know what's going on for further debugging
+      console.log("Something went wrong!", err);
+    }
+  );
+
+  // Close the console input interface since it's not needed anymore
+  rl.close();
+});
